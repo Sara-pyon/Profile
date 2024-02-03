c82e51a402126f25c8bf206b2f98dbbf4522b5e9
Sara Komatsu <radwimps.sara@gmail.com>
2024-02-03 15:07:25 -0700

    Create Logo

---
+++ src/components/Logo.tsx
@@ -0,0 +1,12 @@
+import { Text } from "@chakra-ui/react"
+
+const Logo = () => {
+  return (
+    <Text fontFamily='Lobster' fontSize='2xl'
+          color='yellow.500'>
+        Sara
+    </Text>
+  )
+}
+
+export default Logo
